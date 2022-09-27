4642d6a990e447a6628f673e6c1a96a7573b78b1
Damian-luis <damian.luis.porta@gmail.com>
2022-09-27 18:00:37 -0300

    quitar estads

--- src/componentes/home.jsx
+++ src/componentes/home.jsx
@@ -102,46 +102,49 @@ useEffect(()=>{
             </div>
             
             <div className={styles.verusuarioderecha}>
-<form onSubmit={sendcuidador}>
-  <h4>Añadir cuidador</h4>
-  <p>Recuerde que usted puede añadir mas de un cuidador a un paciente</p>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Nombre del cuidador</label>
-    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingresar nombre del cuidador" onChange={namecuidador} value={formcuidador.name}/>
-    <label for="exampleInputEmail1">Apellido del cuidador</label>
-    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingresar apellido del cuidador" onChange={lastnamecuidador} value={formcuidador.lastname}/>
-    <label for="exampleInputEmail1">Correo del cuidador</label>
-    <input type="mail" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingresar correo del cuidador" onChange={mailcuidador} value={formcuidador.mail}/>
-    <label for="exampleInputEmail1">Parentesco con el cliente</label>
-    <input type="mail" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingresar el parentesco con el paciente" onChange={parentescocuidador} value={formcuidador.parentesco}/>
-  </div>
-  <button type="submit" class="btn btn-primary">Registrar cuidador</button>
-</form>
-<div>
-  <h4>Lista de cuidadores designados</h4>
-  <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Nombre</th>
-      <th scope="col">Apellido</th>
-      <th scope="col">Parentesco</th>
-    </tr>
-  </thead>
-  <tbody>
-  {cuidadores.length>0&&cuidadores.map(e=>{return(
-      <tr>
-      <th scope="row">3</th>
-      <td>{e.name}</td>
-      <td>{e.lastname}</td>
-      <td>{e.parentesco}</td>
-    </tr>)
-  })}
-    
-  </tbody>
-</table>
-  
-  </div>
+              
+              {/*
+              <form onSubmit={sendcuidador}>
+                <h4>Añadir cuidador</h4>
+                <p>Recuerde que usted puede añadir mas de un cuidador a un paciente</p>
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Nombre del cuidador</label>
+                  <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingresar nombre del cuidador" onChange={namecuidador} value={formcuidador.name}/>
+                  <label for="exampleInputEmail1">Apellido del cuidador</label>
+                  <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingresar apellido del cuidador" onChange={lastnamecuidador} value={formcuidador.lastname}/>
+                  <label for="exampleInputEmail1">Correo del cuidador</label>
+                  <input type="mail" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingresar correo del cuidador" onChange={mailcuidador} value={formcuidador.mail}/>
+                  <label for="exampleInputEmail1">Parentesco con el cliente</label>
+                  <input type="mail" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingresar el parentesco con el paciente" onChange={parentescocuidador} value={formcuidador.parentesco}/>
+                </div>
+                <button type="submit" class="btn btn-primary">Registrar cuidador</button>
+              </form>
+              <div>
+                <h4>Lista de cuidadores designados</h4>
+                <table class="table">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Apellido</th>
+                    <th scope="col">Parentesco</th>
+                  </tr>
+                </thead>
+                <tbody>
+                {cuidadores.length>0&&cuidadores.map(e=>{return(
+                    <tr>
+                    <th scope="row">3</th>
+                    <td>{e.name}</td>
+                    <td>{e.lastname}</td>
+                    <td>{e.parentesco}</td>
+                  </tr>)
+                })}
+                  
+                </tbody>
+              </table>
+                
+                </div>
+              */}
 </div>
 
 
